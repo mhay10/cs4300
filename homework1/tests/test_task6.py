d5e17463a0d46c5a5b5a7ce17f1e1e4a89af8df5
mhay10 <maxwellhay1@gmail.com>
2025-09-11 19:24:07 +0000

    Add tests to task 6

--- homework1/tests/test_task6.py
+++ homework1/tests/test_task6.py
@@ -1,4 +1,5 @@
 from src import task6
+import tempfile
 import pytest
 
 # Test count_words counts words in provided file correctly
@@ -9,6 +10,23 @@ def test_provided_file():
 def test_file_not_found():
     with pytest.raises(FileNotFoundError):
         task6.count_words("invalid_file.txt")
+
+# Test count_words with custom files
+@pytest.mark.parametrize(
+    "text, result",
+    [
+        ("Hello World!", 2),
+        ("I'm sorry Dave. I'm afraid I can't do that.", 9),
+        ("", 0),
+    ]
+)
+def test_custom_files(text, result):
+    # Create temp file
+    with tempfile.NamedTemporaryFile(mode="w") as f:
+        f.write(text)
+        f.seek(0)
+        assert task6.count_words(f.name) == result
+
 
 # Run tests
 if __name__ == "__main__":
